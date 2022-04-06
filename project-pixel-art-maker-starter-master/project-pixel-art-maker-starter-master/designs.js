2dfc94ab43be69c3b8c2a4b0f47a22c8457ae5c0
Mikrus <37890070+Mikrus@users.noreply.github.com>
2022-04-06 14:49:11 -0500

    Add files via upload

---
+++ project-pixel-art-maker-starter-master/project-pixel-art-maker-starter-master/designs.js
@@ -0,0 +1,11 @@
+var PixelCanvas = {};
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+
+// Your code goes here!
+
+}
