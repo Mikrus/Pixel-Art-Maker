9554555335e052db0c3974f100d18fa40183917f
Mikrus <37890070+Mikrus@users.noreply.github.com>
2022-04-19 21:01:14 -0500

    Add files via upload
    This is not the final result. It is a draft of what I have been working on and stuck on. I want to upload this just incase.

--- project-pixel-art-maker-starter-master/project-pixel-art-maker-starter-master/designs.js
+++ project-pixel-art-maker-starter-master/project-pixel-art-maker-starter-master/designs.js
@@ -1,11 +1,33 @@
-var PixelCanvas = {};
 // Select color input
+let color = document.getElementById('colorPicker');
 // Select size input
+let sizePicker = document.getElementById('sizePicker');
+// Our canvas
+let canvas = document.getElementById('pixelCanvas');
 
 // When size is submitted by the user, call makeGrid()
+sizePicker.addEventListener("submit", function (event) {
+  event.preventDefault();
+  makeGrid()
+});
 
 function makeGrid() {
+// Must create a variable to store values of height and width.
+let rows = document.getElementById('inputHeight').value;
+let cols = document.getElementById('inputWidth').value;
 
-// Your code goes here!
-
+// For each row
+for (let x = 0; x < row; x++) {
+    // Create row
+    let row = document.createElement('tr');
+    // For each cell
+    for (let z = 0; z < cols; z++) {
+        // Create cell
+        let cell = document.createElement('td');
+        // Add cells to rows
+        row.appendChild(cell);
+    // Add rows to our canvas
+    canvas.appendChild(row);
+    }
+  }
 }
