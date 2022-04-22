7948cb9497f83f385f6f08a560f3d93d564ff28e
Mikrus <37890070+Mikrus@users.noreply.github.com>
2022-04-21 20:09:21 -0500

    Add files via upload
    We have made our table; however, it still has bugs.

--- project-pixel-art-maker-starter-master/project-pixel-art-maker-starter-master/designs.js
+++ project-pixel-art-maker-starter-master/project-pixel-art-maker-starter-master/designs.js
@@ -1,11 +1,37 @@
-var PixelCanvas = {};
 // Select color input
+let color = document.getElementById('colorPicker')
+
+// When the user selects the height and width
+let sizePicker = document.getElementById('sizePicker')
 // Select size input
+let height = document.getElementById('inputHeight')
+let width = document.getElementById('inputWidth')
+
+// Our canvas
+let canvas = document.getElementById('pixelCanvas')
 
 // When size is submitted by the user, call makeGrid()
+sizePicker.addEventListener("click", function (event) {
+  event.preventDefault();
+  makeGrid()
+});
 
 function makeGrid() {
 
-// Your code goes here!
+// For each row
+for (let x = 0; x < width.value; x++) {
+    // Create row
+    let row = document.createElement('tr')
+    // For each cell
+    for (let z = 0; z < height.value; z++) {
+        // Create cell
+        let cell = document.createElement('td')
+        // Add cells to rows
+        row.append(cell)
+        // Add rows to our canvas
+        canvas.append(row)
+    }
+
+  }
 
 }
